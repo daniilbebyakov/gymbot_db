d096d4c19f656a7aa85d81d456444a744a62570c
bebyakovdaniil <bebyakov2014@gmail.com>
2026-02-12 16:20:32 +0300

    add EF

---
+++ Data.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using System.Collections.Generic;
+using System.Xml.Linq;
+namespace GymBot.Data
+{
+    public class GymBotContext : DbContext
+    {
+        public DbSet<Entities.User> Users => Set<Entities.User>();
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseNpgsql("Host=localhost;Port=5432;Username=gymbotuser;Password=gymbotpass;Database=gymbot_db");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Entities.User>().HasIndex(u => u.TgId).IsUnique();
+        }
+    }
+    public class UserRepository
+    {
+        private readonly GymBotContext _context;
+        public UserRepository(GymBotContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> AddUserIfNotExist(long tgId, string username)
+        {
+            bool exists = await _context.Users.AnyAsync(u => u.TgId == tgId);
+            if (exists) return false;
+            try
+            {
+                _context.Users.Add(new Entities.User
+                {
+                    TgId = tgId,
+                    Username = username ?? ""
+                });
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException?.Message);
+            }
+            return true;
+        }
+    }
+}
---
+++ Entities/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GymBot.Data.Entities
+{
+    public class User
+    {
+            public int Id { get; set; }
+            public long TgId { get; set; }
+            public string Username { get; set; } = "";
+    }
+}
