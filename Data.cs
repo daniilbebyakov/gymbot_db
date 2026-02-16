08a1a8b4a36e4f9ffc37d712ff1aa8da7d5e3b75
Zholobov Vladislav <75520011+Jambit0S@users.noreply.github.com>
2026-02-16 19:15:14 +0300

    Небольшой ревью

--- Data.cs
+++ Data.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 using System.Xml.Linq;
 namespace GymBot.Data
 {
+    /// <summary>
+    /// Лучше не хранить два класса в одном файле
+    /// 1 Класс : 1 Файл (Бывают исключения но это обсуждается локально)
+    /// </summary>
     public class GymBotContext : DbContext
     {
         public DbSet<Entities.User> Users => Set<Entities.User>();
         protected override void OnConfiguring(DbContextOptionsBuilder options)
         {
-            options.UseNpgsql("Host=localhost;Port=5432;Username=gymbotuser;Password=gymbotpass;Database=gymbot_db");
+			/// В идеале подобную инфу вынести в некоторый конфиг файл
+			/// Чтобы например
+			/// При запуске проекта можно было без изменения кода выбрать другие данные к БД
+			/// <see cref="https://metanit.com/sharp/efcore/1.5.php"/>
+			options.UseNpgsql("Host=localhost;Port=5432;Username=gymbotuser;Password=gymbotpass;Database=gymbot_db");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -18,7 +26,16 @@ namespace GymBot.Data
     }
     public class UserRepository
     {
+        /// <summary>
+        /// Мб свойство добавить? 
+        /// </summary>
         private readonly GymBotContext _context;
+
+        /// <summary>
+        /// И тип ток с ним общаться.
+        /// </summary>
+        /// public GymBotContext Context => _context;
+
         public UserRepository(GymBotContext context)
         {
             _context = context;
@@ -27,21 +44,39 @@ namespace GymBot.Data
         public async Task<bool> AddUserIfNotExist(long tgId, string username)
         {
             bool exists = await _context.Users.AnyAsync(u => u.TgId == tgId);
-            if (exists) return false;
+            /// Посмотри как описывается код в документации C#
+			/// https://learn.microsoft.com/ru-ru/dotnet/csharp/language-reference/statements/selection-statements
+			/// Кнч все зависит от стандартов компании но скорее всего все будет основываться на деф документации Майков
+			if (exists) 
+            { 
+                return false;
+            }
             try
             {
                 _context.Users.Add(new Entities.User
                 {
                     TgId = tgId,
-                    Username = username ?? ""
+                    Username = username ?? "" // Тут на любителя. Мне больше нрав String.Empty
                 });
                 await _context.SaveChangesAsync();
             }
-            catch (Exception ex)
+            /// !!! В идеале мы должны понимать какое исключение может прийти
+            /// И ловить и обрабаывать так как нужно
+            /// Например, создадим кастомное исключение связанное с тем что пользователь уже сущ в базе бота
+            /// catch (UserAlreadyExist ex) {
+            /// bla-bla-bla;
+            /// }
+            /// 
+            /// Иногда приходится ловить все искл но лучше этого избегать
+            catch (Exception ex) 
             {
-                Console.WriteLine(ex.Message);
+				/// Ок, но можно добавить описания https://learn.microsoft.com/ru-ru/dotnet/api/system.string.format?view=net-10.0
+				/// Там оч много всяких приколюх вот есть на метаните https://metanit.com/sharp/tutorial/7.5.php
+				Console.WriteLine(ex.Message);
                 Console.WriteLine(ex.InnerException?.Message);
             }
+            /// Я любил добавлять пустую строку перед return if  и тп
+            /// Так будто по читаемее ставновится 
             return true;
         }
     }
--- Entities/User.cs
+++ Entities/User.cs
@@ -1,13 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace GymBot.Data.Entities
+﻿namespace GymBot.Data.Entities
 {
-    public class User
-    {
-            public int Id { get; set; }
-            public long TgId { get; set; }
-            public string Username { get; set; } = "";
-    }
+	public class User
+	{
+		/// <summary>
+		/// А нам хватит int?
+		/// Может быть лучше будет uint или еще что-то 
+		/// Я просто не делал ботов не могу точно сказать
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Используй понятные имена. Не сокращай (Только если везде так)
+		/// Либо пиши комментарии на подобие этого 
+		/// просто пропиши 3 слеша и он выдаст обычный summary
+		/// </summary>
+		public long TgId { get; set; } // Тоже самое как про int, но тут почему-то long
+
+		/// <summary>
+		/// Нам важно? Пользователь может сменить имя в любой момент. 
+		/// </summary>
+		public string Username { get; set; } = "";
+	}
+	/// По поводу комментов - Кто-то ЗА кто-то ПРОТИВ
+	/// В идеале "Твой код не требует комментариев" те все понятно и без них
+	/// Посмотри тут "https://learn.microsoft.com/ru-ru/dotnet/csharp/language-reference/language-specification/documentation-comments"
+
 }
